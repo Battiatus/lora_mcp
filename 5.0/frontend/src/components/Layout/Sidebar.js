114c7d320df149152956104926ecca1b7d070603
Battiatus <rametsamatana@yahoo.fr>
2025-06-04 20:17:11 +0200

    fix: Import React in Sidebar component for proper functionality

--- 5.0/frontend/src/components/Layout/Sidebar.js
+++ 5.0/frontend/src/components/Layout/Sidebar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useChatContext } from '../../context/ChatContext';
 import webSocketService from '../../services/webSocketService';
 
